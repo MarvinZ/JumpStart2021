fc966f2d761e86bc07f6f66c7de99234069011bf
Marvin Zumbado <marvinzzz@gmail.com>
2021-01-13 21:15:35 -0600

    fixed client issue but now i have CORS issues

--- src/WebApp/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ src/WebApp/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -9,6 +9,7 @@ export class FetchDataComponent {
   public forecasts: WeatherForecast[];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    baseUrl = 'http://localhost:3000/';
     console.log('weather: get=', baseUrl + 'api/v1/weatherforecasts/Getme');
     http.get<WeatherForecast[]>(baseUrl + 'api/v1/weatherforecasts/Getme').subscribe(result => { // TODO: use generated API client
       console.log('weather: data=', result);
